dbbe98549fa3934634b8ae87d8cb3c1e315dfac6
Juank <jucabapa39@gmail.com>
2024-02-08 14:02:44 -0500

    y angel sigue sin hacer nada

--- src/components/seccionOne.vue
+++ src/components/seccionOne.vue
@@ -1,8 +1,11 @@
 <template>
   <div class="div1Sec2">
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat dolore obcaecati 
-      libero voluptatem reiciendis voluptatum modi. Alias ut et nulla tempora? Tenetur aut 
-      laborum laudantium pariatur molestiae ut, vitae nihil!</p>
+    <img src="../assets/fotoSecOne.png" class="fotoSecOne" alt="Temach">
+      <div class="contenidoSec1">
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat dolore obcaecati 
+        libero voluptatem reiciendis voluptatum modi. Alias ut et nulla tempora? Tenetur aut 
+        laborum laudantium pariatur molestiae ut, vitae nihil!</p>
+      </div>
   </div>
 </template>
 
@@ -15,7 +18,25 @@ export default {
 <style>
 .div1Sec2 {
   width: 100%;
-  height: 100vh;
-  background-color: blue;
+  height: 100%;
+  background-color: rgb(0, 0, 0);
+  padding-top: 3px;
+}
+.fotoSecOne {
+  width: 100%;
+  height: auto;
+  z-index: -1;
+}
+.contenidoSec1 {
+  position: relative;
+  display: flex;
+  justify-content: center; 
+  align-items: center; 
+  color: rgb(255, 255, 255);
+  bottom: 20em; /*Alinea el título verticalmente en el centro */
+  left: 50%; /*Alinea el título horizontalmente en el centro */
+  transform: translate(-50%, -50%);  /*Centra el título*/
+  z-index: 40;
+  width: 40em;
 }
 </style>
